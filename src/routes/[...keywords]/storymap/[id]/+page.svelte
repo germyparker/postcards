208e8cefdd1afd9ad9773f8679cca21cca8a7687
nick3white <nick33white@gmail.com>
2023-08-24 19:53:31 -0500

    adding static sub route and second dynamic route and redirect

---
+++ src/routes/[...keywords]/storymap/[id]/+page.svelte
@@ -0,0 +1,8 @@
+
+<script>
+    import {page} from '$app/stores'
+    const kw = $page.params.keywords || "aint suffin"
+const id = $page.params.id || "still nuffin"
+</script>
+<h1>{kw}</h1>
+<h3>{id}</h3>
